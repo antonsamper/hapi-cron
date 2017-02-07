886bd4fbdc405ddcfa706646378dccbe45b5c731
Anton Samper Rivaya <anton@aeser.co.uk>
2017-02-07 14:15:21 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # hapi-cron
 Cron jobs for internal hapi.js routes
+
+## Installation
+```
+npm install --save hapi-cron
+```
+
+## Usage
+```
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+server.connection();
+
+server.register({
+    register: require('hapi-cron'),
+    options: {
+        jobs: [{
+            name: 'testcron',
+            time: '*/10 * * * * *',
+            timezone: 'Europe/London',
+            request: {
+                method: 'GET',
+                url: '/test-url'
+            },
+            callback
+        }]
+    },
+}, (err) => {
+
+    if (err) {
+        return console.error(err);
+    }
+    
+    server.start(() => {
+    
+        console.info(`Server started at ${ server.info.uri }`);
+    });
+});
+```
+
+## Options
+
+* `name` - [REQUIRED] - The name of the cron job. This must be unique.
+* `time` - [REQUIRED] - A valid cron value. [See cron configuration](#cron-configuration)
+* `timezone` - [REQUIRED] - A valid timezone.
+* `request` - [REQUIRED] - The request object containing the route url path. Other [options](https://hapijs.com/api#serverinjectoptions-callback) can also be passed into the request object.
+    * `url` - [REQUIRED] - Route path to request
+* `callback` - [OPTIONAL] - Callback function to run after the route has been requested. The function will contain the response object from the request.
+
+Please note that the plugin only works when the server contains exactly one connection.
+
+## Cron configuration
+This plugin uses the [node-cron](https://github.com/kelektiv/node-cron) module to setup the cron job. 
+
+### Available Cron patterns:
+
+```
+Asterisk. E.g. *
+Ranges. E.g. 1-3,5
+Steps. E.g. */2
+```
+    
+
+[Read up on cron patterns here](http://crontab.org). Note the examples in the link have five fields, and 1 minute as the finest granularity, but the node cron module allows six fields, with 1 second as the finest granularity.
+
+### Cron Ranges
+
+When specifying your cron values you'll need to make sure that your values fall within the ranges. For instance, some cron's use a 0-7 range for the day of week where both 0 and 7 represent Sunday. We do not.
+
+ * Seconds: 0-59
+ * Minutes: 0-59
+ * Hours: 0-23
+ * Day of Month: 1-31
+ * Months: 0-11
+ * Day of Week: 0-6
+
+
+
+
---
+++ __tests__/index.js
@@ -0,0 +1,237 @@
+'use strict';
+
+
+/*********************************************************************************
+ 1. Dependencies
+ *********************************************************************************/
+
+const CronPlugin = require('../lib');
+const Hapi = require('hapi');
+const Hoek = require('hoek');
+
+
+/*********************************************************************************
+ 2. Exports
+ *********************************************************************************/
+
+describe('registration assertions', () => {
+
+    it('should register plugin without errors', () => {
+
+        const server = new Hapi.Server();
+
+        server.register(CronPlugin, (err) => {
+
+            expect(err).toBeUndefined();
+        });
+    });
+
+    it('should throw error when a job is defined with an existing name', () => {
+
+        const server = new Hapi.Server();
+
+        expect(() => {
+
+            server.register({
+                register: CronPlugin,
+                options: {
+                    jobs: [{
+                        name: 'testname',
+                        time: '*/10 * * * * *',
+                        timezone: 'Europe/London',
+                        request: {
+                            url: '/test-url'
+                        }
+                    }, {
+                        name: 'testname',
+                        time: '*/10 * * * * *',
+                        timezone: 'Europe/London',
+                        request: {
+                            url: '/test-url'
+                        }
+                    }]
+                }
+            }, Hoek.ignore);
+        }).toThrowError('Job name has already been defined');
+    });
+
+    it('should throw error when a job is defined without a name', () => {
+
+        const server = new Hapi.Server();
+
+        expect(() => {
+
+            server.register({
+                register: CronPlugin,
+                options: {
+                    jobs: [{
+                        time: '*/10 * * * * *',
+                        timezone: 'Europe/London',
+                        request: {
+                            url: '/test-url'
+                        }
+                    }]
+                }
+            }, Hoek.ignore);
+        }).toThrowError('Missing job name');
+    });
+
+    it('should throw error when a job is defined without a time', () => {
+
+        const server = new Hapi.Server();
+
+        expect(() => {
+
+            server.register({
+                register: CronPlugin,
+                options: {
+                    jobs: [{
+                        name: 'testcron',
+                        timezone: 'Europe/London',
+                        request: {
+                            url: '/test-url'
+                        }
+                    }]
+                }
+            }, Hoek.ignore);
+        }).toThrowError('Missing job time');
+    });
+
+    it('should throw error when a job is defined with an invalid time', () => {
+
+        const server = new Hapi.Server();
+
+        expect(() => {
+
+            server.register({
+                register: CronPlugin,
+                options: {
+                    jobs: [{
+                        name: 'testcron',
+                        time: 'invalid cron',
+                        timezone: 'Europe/London',
+                        request: {
+                            url: '/test-url'
+                        }
+                    }]
+                }
+            }, Hoek.ignore);
+        }).toThrowError('Time is not a cron expression');
+    });
+
+    it('should throw error when a job is defined without a timezone', () => {
+
+        const server = new Hapi.Server();
+
+        expect(() => {
+
+            server.register({
+                register: CronPlugin,
+                options: {
+                    jobs: [{
+                        name: 'testcron',
+                        time: '*/10 * * * * *',
+                        request: {
+                            url: '/test-url'
+                        }
+                    }]
+                }
+            }, Hoek.ignore);
+        }).toThrowError('Missing job time zone');
+    });
+
+    it('should throw error when a job is defined without a request object', () => {
+
+        const server = new Hapi.Server();
+
+        expect(() => {
+
+            server.register({
+                register: CronPlugin,
+                options: {
+                    jobs: [{
+                        name: 'testcron',
+                        time: '*/10 * * * * *',
+                        timezone: 'Europe/London'
+                    }]
+                }
+            }, Hoek.ignore);
+        }).toThrowError('Missing job request options');
+    });
+
+    it('should throw error when a job is defined without a request object', () => {
+
+        const server = new Hapi.Server();
+
+        expect(() => {
+
+            server.register({
+                register: CronPlugin,
+                options: {
+                    jobs: [{
+                        name: 'testcron',
+                        time: '*/10 * * * * *',
+                        timezone: 'Europe/London',
+                        request: {
+                            method: 'GET'
+                        }
+                    }]
+                }
+            }, Hoek.ignore);
+        }).toThrowError('Missing job request url');
+    });
+});
+
+describe('cron functionality', () => {
+
+    let server;
+    const callback = jest.fn();
+
+    beforeAll((done) => {
+
+        server = new Hapi.Server();
+
+        server.connection({ port: 9000 });
+
+        server.register({
+            register: CronPlugin,
+            options: {
+                jobs: [{
+                    name: 'testcron',
+                    time: '*/10 * * * * *',
+                    timezone: 'Europe/London',
+                    request: {
+                        method: 'GET',
+                        url: '/test-url'
+                    },
+                    callback
+                }]
+            }
+        }, (err) => {
+
+            expect(err).toBeUndefined();
+
+            server.connections[0].inject = jest.fn();
+
+            done();
+        });
+    });
+
+    it('expose access to registered cron jobs vie server.plugins', () => {
+
+        expect(server.plugins['hapi-cron']).toBeDefined();
+        expect(server.plugins['hapi-cron'].jobs.testcron).toBeDefined();
+    });
+
+    it('call internal endpoint every 10 seconds', () => {
+
+        expect(server.connections[0].inject).not.toHaveBeenCalled();
+
+        server.plugins['hapi-cron'].jobs.testcron._callbacks[0]();
+
+        expect(server.connections[0].inject).toHaveBeenCalledWith({
+            method: 'GET',
+            url: '/test-url'
+        }, callback);
+    });
+});
